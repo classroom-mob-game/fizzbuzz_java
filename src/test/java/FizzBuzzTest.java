1c3d0f5a1193bb4dc7a0d1eb8f1fb709796d1a52
Paul Gestwicki <pvgestwicki@bsu.edu>
2022-11-26 11:49:06 -0500

    Initial commit
    This contains a single failing unit test in a gradle project.

---
+++ src/test/java/FizzBuzzTest.java
@@ -0,0 +1,9 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class FizzBuzzTest {
+    @Test
+    public void testFizzBuzz() {
+        Assertions.fail();
+    }
+}
